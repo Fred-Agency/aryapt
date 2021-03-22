b08f1a7213722e45a9fb0a44defc4cd4fcda2295
Donny Adkins <donnyadkins@gmail.com>
2021-03-22 01:37:07 -0400

    Update siteplan.js

--- siteplan.js
+++ siteplan.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 $('.floor, .date, .price').click(function() {
-alert('here hit');
-$( this ).find('.dropdown').toggleClass('hidden');
+$('.dropdown').addClass('hidden');
+$( this ).find('.dropdown').removeClass('hidden');
 });
 });
